5eb3db31780e3a11dd72fe23a58fea952eac745c
SamNewmarco <58578076+SamNewmarco@users.noreply.github.com>
2020-03-12 20:19:07 -0400

    Removed BMP Temp + Add internal LM335

--- Balloon/Balloon.ino
+++ Balloon/Balloon.ino
@@ -1,5 +1,4 @@
 #include <Wire.h>
-#include <Adafruit_BMP280.h>
 #include <Adafruit_Si7021.h>
 #include "hsc_ssc_i2c.h"
 #include <SD.h>
@@ -11,7 +10,8 @@ boolean startupCheck = true;
 int led = 31; //The LED pin
 
 /*lm335 temp setup*/
-#define LM335TEMP_PIN A0 //A0-A5
+#define LM335_IN_PIN A0 //A0-A5
+#define LM335_OUT_PIN A1
 #define LM335UNITS 1 //0 for Kelvin. 1 for Celsius. 2 for Fahrenheit
 #define REF_V 5.02
 
@@ -19,9 +19,9 @@ int led = 31; //The LED pin
 //8 > TX; 7 > RX (can be changed)
 SoftwareSerial mySerial(11, 10);
 Adafruit_GPS GPS(&mySerial);
+#define GPSECHO false
 
 /*Adafruit temp and humidity sensor*/
-Adafruit_BMP280 bmp;
 Adafruit_Si7021 humSensor = Adafruit_Si7021();
 
 /*Geiger Counter Variables*/
@@ -41,12 +41,6 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(GEIGER1_PIN), count_geiger, FALLING);//Interrupt for geiger counter, triggers in falling edge
   
   Serial.begin(9600);
-  //Starts Temp/Pressure sensor
-  if (!bmp.begin()) {
-    Serial.println(F("Could not find temp/pressure sensor"));
-    startupCheck = false;
-    //while(1);
-  }
   //Starts humidity Sensor
   if (!humSensor.begin()) {
     Serial.println(F("Could not find si7021 Sensor"));
@@ -59,23 +53,13 @@ void setup() {
       startupCheck = false;
       //while(1);
   }
-  //Setup for Adafruit temp/hum sensor
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, /*Mode*/
-                  Adafruit_BMP280::SAMPLING_X2, /*Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X4, /*Pressure oversampling*/
-                  Adafruit_BMP280::FILTER_X8,  /* Filtering */
-                  Adafruit_BMP280::STANDBY_MS_500); /*Standby time */
 
   if (!SD.begin()){
     Serial.println(F("SD card reader initialization failed"));
     startupCheck = false;
   }
-  
-  if (lm335Temp(LM335TEMP_PIN, LM335UNITS) < -20 || lm335Temp(LM335TEMP_PIN, LM335UNITS) > 70) {
-    Serial.println(F("Exteranl temperature sensor not found")); 
-    startupCheck = false;
-  }                
-  Serial.println(F("Time(sec):\tClicks per secound:\tInternal Pressure(Pa)\tInternal Temperature(°C)\tRel Humidity:\tExternal Pressure(Pa):\tExternal Temp:\tLatitude:\tLongitude:\tAltitude(m)\tSpeed(knts)\tDirection:\tSatellites:\tTime\n"));
+                  
+  Serial.println(F("Time (sec):\tClicks per secound:\t Internal Pressure (Pa)\tInternal Temperature (°C) \tRel Humidity:\tExternal Pressure (Pa): \t External Temp: \tLatitude: \tLongitude: \tAltitude(m) \tSpeed(knts) \tDirection: \tTime\n"));
 
   //Opens the data file and marks the start
   dataFile = SD.open("data.txt", FILE_WRITE);
@@ -92,7 +76,10 @@ void setup() {
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //this mode cuts out a bunch of the more technical data, if we need more I can change it.
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ); //Update internal data storage every 5 seconds
 
-  
+  if (lm335Temp(LM335_IN_PIN, LM335UNITS) < -20 || lm335Temp(LM335_IN_PIN, LM335UNITS) > 70) {
+    Serial.print(F("Exteranl temperature sensor not found")); 
+    startupCheck = false;
+  }
   
   //If a sensor fails to initialize the led will turn on
   if (!startupCheck) {
@@ -106,9 +93,10 @@ void setup() {
 
  //TODO add checks for each sensor to see if it is connected and working. Also replace checks above ^
 void loop() {
-  GPS.read();
+  char c = GPS.read();
+  if ((c) && (GPSECHO)) Serial.write(c);
   if (GPS.newNMEAreceived()) {
-    GPS.parse(GPS.lastNMEA());
+    if (!GPS.parse(GPS.lastNMEA())) return;
   }
 
   if(millis() - time_start > 5000){
@@ -119,17 +107,14 @@ void loop() {
     unsigned long timeFromStart = millis()/1000;
     /*Gieger counter average over 5 seconds*/
     float geigerCount = avgGeiger();
-    /*Internal pressure from bmp280 sensor*/
-    float intPressure = bmpPressure();
-    /*Internal temperature from bmp280 sensor in celsius*/
-    float intTemp = bmpTemperature();
     /*relatve humidity*/
     float relHumidity = humSensor.readHumidity();
     /*External Pressure from honeywell sensor*/
     float extPressure = honeywellPressure();
     /*External temperature sensor from lm335 sensor.*/
-    float extTemp = lm335Temp(LM335TEMP_PIN, LM335UNITS);
-
+    float extTemp = lm335Temp(LM335_OUT_PIN, LM335UNITS);
+    /*Internal temperature sensor from lm335 sensor.*/
+    float intTemp = lm335Temp(LM335_IN_PIN, LM335UNITS);
     /*Latitude in decimal degrees*/
     float latitude = GPS.latitudeDegrees;
     /*Longitude in decimal degrees*/
@@ -140,8 +125,6 @@ void loop() {
     float gpsSpeed = GPS.speed;
     /*Course in degrees from true north*/
     float angle = GPS.angle;
-    /*number of satalites in use*/
-    int satellites = GPS.satellites;
 
     /*GMT time from gps Hours:Minutes:Seconds*/
     String time = getTime();
@@ -160,12 +143,7 @@ void loop() {
     Serial.print(F("\t"));
     dataFile.print(F("\t"));
     
-    //Writes and prints the internal temperature and pressure from bmp 280
-    Serial.print(intPressure);
-    dataFile.print(intPressure);
-    Serial.print(F("\t"));
-    dataFile.print(F("\t"));
-    
+    //Writes and prints internal temperture from lm335 sensor. 
     Serial.print(intTemp);
     dataFile.print(intTemp);
     Serial.print(F("\t"));
@@ -193,15 +171,15 @@ void loop() {
     dataFile.print(F("\t"));
 
     //Writes and prints latitude in decimal degrees
-    Serial.print(latitude);
-    dataFile.print(latitude);
+    Serial.print(latitude,7);
+    dataFile.print(latitude,7);
 
     Serial.print(F("\t"));
     dataFile.print(F("\t"));
 
     //Writes and prints longitude in decimal degrees
-    Serial.print(longitude);
-    dataFile.print(longitude);
+    Serial.print(longitude,7);
+    dataFile.print(longitude,7);
 
     Serial.print(F("\t"));
     dataFile.print(F("\t"));
@@ -230,21 +208,12 @@ void loop() {
     //Serial.println();
     //dataFile.println();
 
-    //Writes and print the number of satellites in use
-    Serial.print(satellites);
-    dataFile.print(satellites);
-    
-    Serial.print(F("\t"));
-    dataFile.print(F("\t"));
-    
     //Writes and prints the time down to the second in GMT
     Serial.print(time);
     dataFile.print(time);
-    
-    Serial.println();
-    dataFile.println();
 
-   
+    Serial.println();
+    dataFile.println();    
 
     geiger_ct = 0;
   }
@@ -280,22 +249,6 @@ float avgGeiger(){
  
 void count_geiger(void){
   geiger_ct++;//Count up on falling edge 
-}
-
-/**
- * Gets pressure from BM280 sensor
- * @return  pressure in Pascals
- */
-float bmpPressure(){
-  return bmp.readPressure();
-}
-
-/**
- * Gets temperature from BMP280 sensor
- * @return Temperature in Celsius
- */
-float bmpTemperature(){
-  return bmp.readTemperature();
 }
 
 //Returns temperture from LM335 sensor.
